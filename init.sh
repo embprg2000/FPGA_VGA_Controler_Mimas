6d8e90583dcd9c8f2f23a0790da5e1988c34a517
Evert H Rozendaal <emb.prg2000@gmail.com>
2016-06-29 10:27:03 -0500

    Delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-echo "# FPGA_VGA_Controler_Mimas" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/embprg2000/FPGA_VGA_Controler_Mimas.git
-git push -u origin master
